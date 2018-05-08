f500b3c560d18b8e947ed1ed55b0ca636345f95a
joel <iamjoel007@gmail.com>
2018-05-08 15:17:44 +0800

    add select

--- src/views/component/Select.vue
+++ src/views/component/Select.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="main">
+    选择水果(单选):
+
+    选择水果(多选):
+    
+    用户数据(远程数据):
   </div>
 </template>
 
@@ -7,11 +12,31 @@
 export default {
   data() {
     return {
-
+      list: [{
+        label: '苹果',
+        value: 1,
+      },{
+        label: '梨',
+        value: 2,
+      },{
+        label: '香蕉',
+        value: 3,
+      },{
+        label: '荔枝',
+        value: 4,
+      }],
+      remoteList: []
     }  
   },
   methods: {
-    
+    fetchRemoteList() {
+      this.$http.get('https://jsonplaceholder.typicode.com/users').then(data => {
+        // 你的代码在这里
+      })
+    }
+  },
+  mounted() {
+    this.fetchRemoteList()
   }
 }
 </script>
---
+++ task/component/select.md
@@ -0,0 +1,8 @@
+# 下拉框(Select 选择器)
+路由 `/component/select`  
+源码地址 `@/src/views/component/Select.vue`
+
+要求
+1. 做 选择水果的下拉框，单选。下拉数据来自 `list`。
+1. 做 选择水果的下拉框，多选。下拉数据来自 `list`。
+1. 做 选择用户的下拉框，单选。下拉数据来自 `remoteList`。 `remoteList` 是 `fetchRemoteList` 获取到的。
