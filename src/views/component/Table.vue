2980303eade757a8677681d4f745a2871b36f16d
zhuangxuchi <1617225642@qq.com>
2019-05-14 21:10:43 +0800

    Update Table.vue

--- src/views/component/Table.vue
+++ src/views/component/Table.vue
@@ -1,25 +1,14 @@
 <template>
   <div class="main">
-    <el-table
-      :data="list"
-      border
-      align="center"
-      stripe>
-      <el-table-column
-        type="index"
-        label="序列"
-        align="center"
-        width="80">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        >
-      </el-table-column>
-      <el-table-column
-        prop="gender"
-        label="性别"
-        >
+    <el-table :data='list' border align='center' stripe>
+      <el-table-column type='index' label='序列' align='center' width='80'></el-table-column>
+      <el-table-column prop='name' label='姓名'></el-table-column>
+      <el-table-column prop='gender' label='性别'>
+        <template scope="scope">
+          <span v-if="scope.row.gender===1" style="color:red">男</span>
+          <span v-else-if="scope.row.gender===2" style="color:green">女</span>
+          <span v-else>未知</span>
+        </template>
       </el-table-column>
     </el-table>
   </div>
@@ -29,20 +18,20 @@
 export default {
   data() {
     return {
-      list: [{
-        name: 'Joel',
-        gender: 1
-      }, {
-        name: 'Mary',
-        gender: 2
-      }, {
-        name: 'Jimmy',
-        gender: 3
-      }, ]
-    }  
+      list:[{
+        name:'Joel',
+        gender:1
+      },{
+        name:'Mary',
+        gender:2
+      },{
+        name:'Jimmy',
+        gender:3
+      }]
+    }
   },
-  methods: {
-    
+  methods:{
+
   }
 }
 </script>
