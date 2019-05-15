00a3b0ae1168c1c43974449bba4aeaceba263153
zhuangxuchi <1617225642@qq.com>
2019-05-15 20:37:30 +0800

    Update Button.vue

--- src/views/component/Button.vue
+++ src/views/component/Button.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="main">
+    <el-row>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <br><br>
+      <el-button round>大</el-button>
+      <el-button size="medium" round>中</el-button>
+      <el-button size="small" round>小</el-button>
+      <el-button size="mini" round>超小</el-button>
+      <br><br>
+      <el-button type="text">文字按钮</el-button>
+      <el-button type="text" disabled>禁用按钮</el-button>
+    </el-row>
   </div>
 </template>
 
