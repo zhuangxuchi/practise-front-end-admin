4ee3337b0a04972f0379e1e8ff33d3ee2a82fbaa
zhuangxuchi <1617225642@qq.com>
2019-05-15 18:22:52 +0800

    Update Message.vue

--- src/views/component/Message.vue
+++ src/views/component/Message.vue
@@ -1,23 +1,48 @@
 <template>
-  <div class="main">
-    <el-button :plain="true">成功</el-button>
-    <el-button :plain="true">警告</el-button>
-    <el-button :plain="true">消息</el-button>
-    <el-button :plain="true">错误</el-button>
+  <div>
+    <el-button :plain="true" @click="open2">成功</el-button>
+    <el-button :plain="true" @click="open3">警告</el-button>
+    <el-button :plain="true" @click="open">信息</el-button>
+    <el-button :plain="true" @click="open4">错误</el-button>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
+  export default {
+    methods: {
+      open() {
+        this.$message({
+          message:'这是一条消息提示',
+          showClose: true,
+          center: true
+          });
+      },
+      open2() {
+        this.$message({
+          showClose: true,
+          message: '恭喜你，这是一条成功消息',
+          type: 'success',
+          center: true
+        });
+      },
 
+      open3() {
+        this.$message({
+          showClose: true,
+          message: '警告哦，这是一条警告消息',
+          type: 'warning',
+          center: true
+        });
+      },
+
+      open4() {
+        this.$message({
+          showClose: true,
+          message:'错了哦，这是一条错误消息',
+          type:'error',
+          center: true
+          });
+      }
     }
-  },
-  methods: {
-    
   }
-}
 </script>
-
-<style scoped></style>
