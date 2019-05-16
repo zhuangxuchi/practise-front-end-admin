03d397ca0d0b2757f1a4f15602f26445d229b1fe
zhuangxuchi <1617225642@qq.com>
2019-05-16 17:51:53 +0800

    Update Switch.vue

--- src/views/component/Switch.vue
+++ src/views/component/Switch.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="main">
+      <el-switch
+        v-model="value4"
+        active-color="#13ce66"
+        inactive-color="#ff4949"
+        active-text="关"
+        inactive-text="开"
+        active-value=1
+        inactive-value=0>
+      </el-switch>
   </div>
 </template>
 
@@ -7,7 +16,7 @@
 export default {
   data() {
     return {
-
+      value4: true
     }  
   },
   methods: {
