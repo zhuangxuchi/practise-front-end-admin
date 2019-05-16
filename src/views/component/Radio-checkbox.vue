f265f4b183baab3eed7d425d2eb4de405b64e9a0
zhuangxuchi <1617225642@qq.com>
2019-05-16 17:51:50 +0800

    Update Radio-checkbox.vue

--- src/views/component/Radio-checkbox.vue
+++ src/views/component/Radio-checkbox.vue
@@ -1,13 +1,29 @@
 <template>
+<div>
   <div class="main">
-  </div>
+    <el-radio-group v-model="radio2">
+    <el-radio :label="1">A</el-radio>
+    <el-radio :label="2">B</el-radio>
+    <el-radio :label="3">C</el-radio>
+  </el-radio-group>
+  </div><br>
+  <div class="main">
+    <el-checkbox-group v-model="checkList">
+      <el-checkbox label="A"></el-checkbox>
+      <el-checkbox label="B"></el-checkbox>
+      <el-checkbox label="C"></el-checkbox>
+      <el-checkbox label="D"></el-checkbox>
+    </el-checkbox-group>
+  </div>  
+</div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-
+      radio2:'0',
+      checkList:[]
     }  
   },
   methods: {
