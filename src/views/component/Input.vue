f49c02262d1dd82ba23b21fd78ac730cef4db2fb
zhuangxuchi <1617225642@qq.com>
2019-05-16 12:52:22 +0800

    Update Input.vue

--- src/views/component/Input.vue
+++ src/views/component/Input.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="main">
+    <div class="shurukuang">
+      <el-input v-model="input" placeholder="请输入内容"></el-input>
+    </div>
+    <br>
+    <div class="wenbenyu">
+      <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="textarea"></el-input>
+    </div>
+    <br>
+    <div class="shurukuang">
+    <el-input v-model="num1" type="number" placeholder="请输入数字"></el-input>
+    </div>
   </div>
 </template>
 
@@ -7,7 +18,9 @@
 export default {
   data() {
     return {
-
+      input: '',
+      textarea:'',
+      num1:''
     }  
   },
   methods: {
@@ -16,4 +29,11 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.shurukuang{
+  width: 200px;
+}
+.wenbenyu{
+  width: 420px;
+}
+</style>
