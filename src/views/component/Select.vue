6a53eb55f5f1d3ea293c1bc4bff0ad78f2ea0a39
zhuangxuchi <1617225642@qq.com>
2019-05-21 19:15:58 +0800

    Update Select.vue

--- src/views/component/Select.vue
+++ src/views/component/Select.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="main">
     选择水果(单选):
-
+    <el-select v-model="value1" placeholder="请选择">
+      <el-option
+        v-for="item in list"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
+    <br><br>
     选择水果(多选):
-    
+    <el-select v-model="value2" multiple placeholder="请选择">
+      <el-option
+        v-for="item in list"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
+    <br><br>
     用户数据(远程数据):
+    <el-select v-model="value3" placeholder="请选择">
+      <el-option
+        v-for="item in remoteList"
+        :key="item.id"
+        :label="item.name"
+        :value="item.id">
+      </el-option>
+    </el-select>
   </div>
 </template>
 
@@ -25,13 +49,16 @@ export default {
         label: '荔枝',
         value: 4,
       }],
-      remoteList: []
+      value1: '',
+      value2:'',
+      value3: [],
+      remoteList:[]
     }  
   },
   methods: {
     fetchRemoteList() {
       this.$http.get('https://jsonplaceholder.typicode.com/users').then(data => {
-        // 你的代码在这里
+        this.remoteList=data.data
       })
     }
   },
